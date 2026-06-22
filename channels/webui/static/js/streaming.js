8089e1896d04462711185e28e9a10749def9f2fa
Rose22 <rosemelody22@gmail.com>
2026-06-22 20:37:20 +0200

    [webui] remove insane amount of token usage polls during toolcall streaming

--- channels/webui/static/js/streaming.js
+++ channels/webui/static/js/streaming.js
@@ -454,9 +454,6 @@ function handleToolCallDelta(data, aiMsgDiv, aiWrapper) {
 
         renderStreamingToolCall(index, streamingToolCalls[index], aiMsgDiv);
     }
-
-    // Update token usage as the assistant's tool call message has been added to context
-    updateTokenUsage();
 }
 
 /**
--- channels/webui/sw.js
+++ channels/webui/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'openlumara-v7.2.0';
+const CACHE_NAME = 'openlumara-v7.2.1';
 const ASSETS = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
